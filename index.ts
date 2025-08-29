0ce19512711a48212e72a3fa54946b2a94f53569
home <likestim1989@gmail.com>
2025-08-29 13:49:52 +0800

    Initial commit

---
+++ index.ts
@@ -0,0 +1,51 @@
+import { Events, Menu, LocalPlayer, GameState, EventsSDK, ImageData } from 'github.com/octarine-public/wrapper/index'
+
+const spinTDInjectEntry = Menu.AddEntry('SpinTD Inject 💫', ImageData.Icons.icon_analytics, tooltip='Inject SpinTD features into the game')
+let customGameNode: Menu.Node | null = null
+customGamesEntry.AddShortDescription('supported_games')
+
+EventsSDK.on('GameStarted', () => {
+    switch (GameState.AddonName) {
+        case '2860562213': {
+            const spinTDInjectEntry = Menu.AddEntry('SpinTD Inject 💫', ImageData.Icons.icon_analytics, tooltip='Inject SpinTD features into the game')
+
+            customGameNode = customGamesEntry.AddNode('custom_spintd')
+            const valuesPlayers = ["1", "2", "3", "4"]
+            customGameNode.AddDropdown("Select player", valuesPlayers, 0, "Select player to give gold")
+            const goldSlider = customGameNode.AddSlider('Gold amount', 0, 0, 500, 0, 'Amount of gold to give', 1)
+            const giveGoldButton = customGameNode.AddButton('Give gold', 'Give gold to yourself', 1)
+
+            giveGoldButton.OnValue(() => {
+                const gold = goldSlider.value
+
+                CustomGameEvents.FireEventToServer('DebugAddGold', new Map<string, any>(Object.entries({
+                    player_id: LocalPlayer!.PlayerID,
+                    gold_number: gold
+                })))
+            })
+
+            const allowBuy = customGameNode.AddToggle('Debug Buy', false, 'Ability of giving items from store for free', 2)
+
+            allowBuy.OnValue(() => {
+                CustomGameEvents.FireEventToServer('EnableDebugBuy', new Map<string, any>(Object.entries({
+                    player_id: LocalPlayer!.PlayerID,
+                    debug_buy: allowBuy.value ? 1 : 0
+                })))
+            })
+
+            break
+        }
+
+        default: {
+            break
+        }
+    }
+})
+
+EventsSDK.on('GameEnded', () => {
+    customGameNode?.DetachFromParent()
+})
+
+Events.on('CustomGameEvent', (name, data) => {
+    console.log('custom event', { name, data })
+})
