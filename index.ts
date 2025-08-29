6bb9324127ac7d8a83f802c4783003a27a425dbf
home <likestim1989@gmail.com>
2025-08-29 14:21:45 +0800

    Add custom games entry to menu and modify gold giving functionality

--- index.ts
+++ index.ts
@@ -4,11 +4,15 @@ const customGamesEntry = Menu.AddEntry('Custom Games HAX', ImageData.Icons.icon_
 
 let customGameNode: Menu.Node | null = null
 customGamesEntry.AddShortDescription('Supported Games', '', 0,ImageData.Icons.transfer_arrow_png)
+customGameNode = customGamesEntry.AddNode('SpinTD', ImageData.Icons.gold_large, 'Inject SpinTD features into the game')
+const playerIDDrowDown = customGameNode.AddDropdown('Player ID', ["1","2"],0,'Select player ID to give gold',0)
+const goldSlider = customGameNode.AddSlider('Gold amount', 0, 0, 500, 0, 'Amount of gold to give', 1)
+const giveGoldButton = customGameNode.AddButton('Give gold', 'Give gold to yourself', 1)
 
 EventsSDK.on('GameStarted', () => {
     switch (GameState.AddonName) {
         case '2860562213': {
-            customGameNode = customGamesEntry.AddNode('SpinTD Inject', ImageData.Icons.gold_large, 'Inject SpinTD features into the game')
+            customGameNode = customGamesEntry.AddNode('SpinTD', ImageData.Icons.gold_large, 'Inject SpinTD features into the game')
 
             const goldSlider = customGameNode.AddSlider('Gold amount', 0, 0, 500, 0, 'Amount of gold to give', 1)
             const giveGoldButton = customGameNode.AddButton('Give gold', 'Give gold to yourself', 1)
