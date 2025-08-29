bc578fcea3c2daa7fcfe709fdf17e93a1778a9a9
home <likestim1989@gmail.com>
2025-08-29 14:17:14 +0800

    Add custom games entry to menu and modify gold giving functionality

--- index.ts
+++ index.ts
@@ -1,23 +1,15 @@
 import { Events, Menu, LocalPlayer, GameState, EventsSDK, ImageData } from 'github.com/octarine-public/wrapper/index'
 
-const spinTDEntry = new Menu.AddEntry('SpinTD Reborn', ImageData.Icons.gold_large)
-spinTDEntry.AddShortDescription('Game not loaded','SpinTD Reborn features will appear when you enter the game')
-let customGameNode: Menu.Node | null = null
+const customGamesEntry = Menu.AddEntry('Custom Games', ImageData.Icons.icon_svg_keyboard)
 
-let is_closed = false;
-let buttonTest = spinTDEntry.AddButton('Change state', 'Change state of SpinTD menu', 1)
-buttonTest.OnValue(() => {
-    changeStateMenu(!is_closed);
-    is_closed = !is_closed;
-});
+let customGameNode: Menu.Node | null = null
+customGamesEntry.AddShortDescription('Supported Games', 'List of supported custom games with additional features', 1,ImageData.Icons.chat_arrow_down)
 
 EventsSDK.on('GameStarted', () => {
     switch (GameState.AddonName) {
         case '2860562213': {
-            const spinTDInjectEntry = Menu.AddEntry('SpinTD', ImageData.Icons.icon_courier, tooltip='Inject SpinTD features into the game')
+            customGameNode = customGamesEntry.AddNode('SpinTD Inject', ImageData.Icons.gold_large, 'Inject SpinTD features into the game')
 
-            customGameNode = customGamesEntry.AddNode('custom_spintd')
-            //customGameNode.AddDropdown()
             const goldSlider = customGameNode.AddSlider('Gold amount', 0, 0, 500, 0, 'Amount of gold to give', 1)
             const giveGoldButton = customGameNode.AddButton('Give gold', 'Give gold to yourself', 1)
 
@@ -52,28 +44,6 @@ EventsSDK.on('GameEnded', () => {
     customGameNode?.DetachFromParent()
 })
 
-async function changeStateMenu(is_closed:boolean = false){
-    if(is_closed){
-        let entry1 = new Menu.AddEntry('SpinTD Reborn', ImageData.Icons.gold_large)
-        entry1.AddShortDescription('1.Game not loaded','SpinTD Reborn features will appear when you enter the game')
-        entry1.DELETE();
-        let buttonTest = entry1.AddButton('Change state', 'Change state of SpinTD menu', 1)
-        buttonTest.OnValue(() => {
-            changeStateMenu(is_closed);
-            is_closed = !is_closed;
-        });
-    } else {
-        let entry2 = new Menu.AddEntry('SpinTD Reborn', ImageData.Icons.gold_large)
-        entry2.DELETE();
-        entry2.AddShortDescription('2.Game not loaded','SpinTD Reborn features will appear when you enter the game')
-        let buttonTest = entry2.AddButton('Change state', 'Change state of SpinTD menu', 1)
-        buttonTest.OnValue(() => {
-            changeStateMenu(is_closed);
-            is_closed = !is_closed;
-        });
-
-    }
-}
 Events.on('CustomGameEvent', (name, data) => {
     console.log('custom event', { name, data })
 })
